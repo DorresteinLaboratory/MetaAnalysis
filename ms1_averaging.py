5e01dc7a2a7abd7ec0e26cac23531cd14a6c9da7
Ming Wang <mwang87@gmail.com>
2019-01-18 14:11:45 -0800

    refactring ms1 averaging code

--- ms1_averaging.py
+++ ms1_averaging.py
@@ -1,61 +1,63 @@
-import scipy
+#!/usr/bin/python
 import numpy as np
 from pyteomics import mzxml
-from line_profiler import LineProfiler
-import copy 
 import pandas as pd
-from itertools import izip
+import copy
 import os
-from __future__ import division
-%reload_ext line_profiler%reload_ext line_profiler
+import sys
+import getopt
+import os
 
-mass_list = []
-intensity_list = []
+#Bucket Spectra into a vector
+#Norms it too
+def vectorize_peaks(peaks, max_mass, bin_size, normalize=False, sqrt_peaks=True):
+    number_of_bins = int(max_mass / bin_size)
+    peak_vector = [0.0] * number_of_bins
 
-path = (r'H:\Chrissy\Averaging_Study\mzXML\101_Prebiotic_CDR_SD_1.mzXML')
-spectra = mzxml.read(path, read_schema = True) #type is pyteomics mzxml   
+    for peak in peaks:
+        mass = peak[0]
+        bin_index = int(mass/bin_size)
+        if bin_index > number_of_bins - 1:
+            continue
+        peak_vector[bin_index] += peak[1]
 
-def timing_stuff():
-    inten2 = list() 
-    mlist2 = list()
-    mlist = np.array([])
-    inten = np.array([])
-    final_mlist = np.array([])
-    final_inten = np.array([])
-    
+    np_array = np.array(peak_vector)
+
+    if sqrt_peaks == True:
+        np_array = np.sqrt(np_array)
+
+    if normalize:
+        np_array = np.true_divide(np_array, np.linalg.norm(np_array))
+
+    return np_array
+
+def average_ms1(input_filename, output_filename=None, bin_width=1.0, format="csv"):
+    mass_list = []
+    intensity_list = []
+
+    spectra = mzxml.read(input_filename, read_schema = True) #type is pyteomics mzxml
+
+    peaks_list = []
+
     for element in spectra:
-        mlist = copy.deepcopy(element['m/z array']) 
+        mlist = copy.deepcopy(element['m/z array'])
         inten = copy.deepcopy(element ['intensity array'])
-              
-        if mlist[0] >= 99:
-            mlist = mlist.tolist() 
-            inten = inten.tolist()
-        
-            mlist2.extend(mlist)
-            inten2.extend(inten)
-        mlist = []
-        inten = []
-        
-    final_mlist = np.unique(mlist2, return_inverse = True)
-    length = final_mlist[0].shape[0]
-    
-    final_inten = np.zeros(length) 
-    
-    final_inten = final_inten.tolist()
-    indices = list(final_mlist[1])
-    
-    for intensity, index in zip(inten2, indices): #time heavy processes
-        final_inten[index] += intensity 
-        
-    final_mlist = final_mlist[0]
-    
-    for counter, item2 in enumerate(final_inten):
-        if item2 == 0:
-            final_mlist[counter] = 0
-        
-    final_mlist = [value for value in final_mlist if value != 0]
-    final_inten = [value for value in final_inten if value != 0]
-    d = {'mlist':final_mlist, 'inten': final_inten}
-    df = pd.DataFrame(data = d)
-    df.to_csv(r'H:\Chrissy\Averaging_Study\OUTPUT_mzXML_file_process_code\OUTPUT_101_Prebiotic_CDR_SD_1.csv')
-%lprun -f timing_stuff timing_stuff()
+        mslevel = element["msLevel"]
+        if mslevel != 2:
+            continue
+
+        peaks_list += zip(mlist, inten)
+
+    numpy_vector = vectorize_peaks(peaks_list, 2000, bin_width)
+
+    if output_filename != None:
+        dt = pd.DataFrame(data=numpy_vector)
+        dt.to_csv(output_filename, mode='a', index=True)
+
+    return numpy_vector
+
+def main():
+    average_ms1(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+    main()
